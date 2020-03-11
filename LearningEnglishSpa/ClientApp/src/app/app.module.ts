f36c632c1440460e182de3130301fe9227236c49
hd <kur1l0@yandex.ru>
2020-03-11 18:31:27 +0300

    adding identity to angular

--- LearningEnglishSpa/ClientApp/src/app/app.module.ts
+++ LearningEnglishSpa/ClientApp/src/app/app.module.ts
@@ -5,30 +5,29 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { Identity } from './components/identity.component';
+import { SecurityService } from './services/security.service';
+import { ConfigurationService } from './services/configuration.service';
+import { StorageService } from './services/storage.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent
+    Identity
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
+      { path: '', component: HomeComponent, pathMatch: 'full' }
     ])
   ],
-  providers: [],
+  providers: [SecurityService,
+    ConfigurationService,
+    StorageService,],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ LearningEnglishSpa/ClientApp/src/app/services/configuration.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from "@angular/common/http";
+import { IConfiguration } from '../models/configuration.model';
+import { StorageService } from './storage.service';
+import { Observable, Subject } from 'rxjs';
+
+@Injectable()
+export class ConfigurationService {
+  serverSettings: IConfiguration;
+  // observable that is fired when settings are loaded from server
+  private settingsLoadedSource = new Subject();
+  settingsLoaded$ = this.settingsLoadedSource.asObservable();
+  isReady: boolean = false;
+
+  constructor(private http: HttpClient, private storageService: StorageService) { }
+
+  load() {
+    const baseURI = document.baseURI.endsWith('/') ? document.baseURI : `${document.baseURI}/`;
+    let url = `${baseURI}Home/Configuration`;
+    this.http.get<IConfiguration>(url).subscribe((response) => {
+      this.serverSettings = response;
+      this.storageService.store('identityUrl', this.serverSettings.identityUrl);
+      this.isReady = true;
+      this.settingsLoadedSource.next();
+    });
+  }
+}
